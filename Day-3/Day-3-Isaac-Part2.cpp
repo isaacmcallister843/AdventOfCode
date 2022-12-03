aa4386926f40b784b1cdad5f7859b0aa35808277
isaacmcallister843 <isaac.mcallister@gmail.com>
2022-12-03 14:05:02 -0800

    better

--- Day-3/Day-3-Isaac-Part2.cpp
+++ Day-3/Day-3-Isaac-Part2.cpp
@@ -12,10 +12,8 @@ std::set<char> GetAllCommonValues(std::string firstString, std::string secondStr
     std::set<char> firstSet = convertToSet(firstString);
     std::set<char> secondSet = convertToSet(secondString);
     std::set<char> thirdSet = convertToSet(thirdString);
-    
     std::set<char> firstIntersection;  
     std::set<char> secondIntersection;
-
     std::set_intersection(begin(firstSet), end(firstSet), begin(secondSet), end(secondSet), inserter(firstIntersection, end(firstIntersection)));
     std::set_intersection(begin(firstIntersection), end(firstIntersection), begin(thirdSet), end(thirdSet), inserter(secondIntersection, end(secondIntersection)));
 
@@ -26,52 +24,33 @@ std::set<char> GetAllCommonValues(std::string firstString, std::string secondStr
 
 int main(){
     
-
     std::string line;
     std::ifstream input("Day-3-Input.txt");
     std::string firstItems, secondItems, thirdItems;
 
+    std::vector<std::string>threeItems; 
+
     int priorityValue;
     int runningTotal = 0; 
-    int lineCounter = 0; 
     
     while (std::getline(input, line)) {
         
-        switch (lineCounter)
-        {
-            case 0:
-                firstItems = line; 
-                lineCounter++;
-                break;
-            case 1: 
-                secondItems = line;
-                lineCounter++; 
-                break; 
-            case 2: 
-                thirdItems = line; 
-                
-                std::set<char> commonValues = GetAllCommonValues(firstItems, secondItems, thirdItems);
-                
-                std::set<char>::iterator location;  
-
-                for(location=commonValues.begin(); location!=commonValues.end(); ++location){
-                    
-                    char commonValue_ = *location;
-                    std::cout<<commonValue_<<std::endl;
-                    int ASCII_Code = int(commonValue_);
-                    if(ASCII_Code < 91){
-                        priorityValue = ASCII_Code - 38;
-                    }
-                    else{
-                        priorityValue = ASCII_Code - 96;
-                    }
-                    runningTotal += priorityValue;
-                }
-                lineCounter=0;
-
-        }
-        
+        threeItems.push_back(line);
+        if(size(threeItems)==3){
             
+            std::set<char> commonValues = GetAllCommonValues(threeItems[0], threeItems[1], threeItems[2]);
+            std::set<char>::iterator location;  
+
+            for(location=commonValues.begin(); location!=commonValues.end(); ++location){
+                char commonValue_ = *location;
+                int ASCII_Code = int(commonValue_);
+                if(ASCII_Code < 91){priorityValue = ASCII_Code - 38;}
+                else{priorityValue = ASCII_Code - 96;}
+                runningTotal += priorityValue;
+            }
+
+            threeItems.clear();
+        }       
     }
     
     std::cout<< "Part 2s: "<< runningTotal<<std::endl; 
